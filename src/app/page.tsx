c612d7f68e64f81bc523396f5cb9cbb798fe512d
EngJao89 <eng_joao@outlook.com>
2024-06-27 04:46:19 -0300

    feat(dma02-select-pages): implementa seletor de páginas com radix
    BREAKING CHANGE: Change affects Home layout and spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { SubMenu } from "@/components/SubMenu";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Dev Motors</h1>
+      <SubMenu />
     </main>
   );
 }
